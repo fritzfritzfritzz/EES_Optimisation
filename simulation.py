e16a7a7e887416914d91714830d93904f99e031e
Fritz <fritz@Fritzs-Air.fritz.box>
2023-07-13 00:47:29 +0200

    a few more changes to the simulation.py example

--- simulation.py
+++ simulation.py
@@ -4,12 +4,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 import time
 
+st.set_page_config(layout="wide")
 # Create a sample dataframe
 df_solar_pred = pd.read_pickle('data/solar_predictions.pkl')
 
 progress_bar = st.progress(0)
 status_text = st.empty()
-
+plot_type = st.sidebar.selectbox("Select plot type", ('bar', 'line', 'scatter'))
 
 for t in range(10):
     if t == 0:
@@ -33,10 +34,10 @@ st.subheader("Dataframe")
 
 # Plot the dataframe
 st.subheader("Plot")
-plot_type = st.selectbox("Select plot type", ('bar', 'line', 'scatter'))
+#plot_type = st.selectbox("Select plot type", ('bar', 'line', 'scatter'))
 plot = st.empty()
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=(20,7))
 # Removing ticks and labels
 
 # Setting the axis limits
@@ -56,6 +57,9 @@ for i in range(num_iterations):
     plt.scatter(x=result[i][0],y=result2[i][0])
     plt.xlim((result[i][0][0],result[i][0][-1]))
     plt.ylim(-1000, 5000)
+    plt.ylabel('predicted power [kW]')
+    plt.xlabel('time')
+    plt.title('Solar Prediction')
 
 
     # Pause for simulation speed
@@ -63,7 +67,7 @@ for i in range(num_iterations):
     plot.pyplot(fig)
     #plot.line_chart(random_walk[:i+1])
     # Pause for simulation speed
-    time.sleep(2)
+    time.sleep(1)
 
 
 # Display the plot
